7209463110ab34699b247fa0f8ba4cd1405a1b9e
Takuya Takamine <tatakusub@gmail.com>
2024-04-12 11:10:54 +0900

    画像処理用のinterfaceの追加

---
+++ app/src/main/java/com/stellarcoders/IImageProcessor.java
@@ -0,0 +1,14 @@
+package com.stellarcoders;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface IImageProcessor {
+
+    /**
+     * detectItems
+     * ClassNum / Quantity
+     * @return
+     */
+    HashMap<Integer, Integer> detectItems() ;
+}
